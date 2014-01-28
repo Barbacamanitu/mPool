c7e9b0d0e5fdfeedaabb033143267062139ac7e3
Russ Martelli <russmbiz@gmail.com>
2014-01-28 10:32:08 -0600

    Return

--- Pool/Pool.cpp
+++ Pool/Pool.cpp
@@ -12,11 +12,13 @@ private:
 	void * mPool;
 	std::vector<int> mAvailableIndices;
 	size_t mObjectSize;
+	int mCount;
 
 public:
 	void Allocate(int count, size_t size)
 	{
 		mObjectSize = size;
+		mCount = count;
 		mPool = malloc(count * size);
 		mAvailableIndices.reserve(count);
 		for (int i = 0; i < count; i++)
@@ -42,6 +44,15 @@ public:
 		T* newObj = new (GetOffset(index)) T;
 		return newObj;
 	}
+
+	void Return(void * ptr)
+	{
+		intptr_t offset = (intptr_t)ptr - (intptr_t)mPool;
+		if (offset < 0 || offset >= mCount)
+			return;
+
+		mAvailableIndices.push_back(offset);
+	}
 };
 
 class MyObject
@@ -60,8 +71,29 @@ int _tmain(int argc, _TCHAR* argv[])
 	Pool pool;
 	pool.Allocate(10, sizeof(MyObject));
 
-	MyObject* mObj = pool.Get<MyObject>();
-	std::cout << mObj->a;
+	std::vector<MyObject*> objects;
+
+	int nulled = 0;
+	for (int i = 0; i < 15; i++)
+	{
+		MyObject* obj = pool.Get<MyObject>();
+		if (obj != nullptr)
+		{
+			objects.push_back(obj);
+			if (rand() % 100 > 50)
+			{
+				pool.Return(objects.back());
+				objects.pop_back();
+			}
+		}
+		else
+			nulled++;
+
+		
+	}
+
+	std::cout << "Objects Created: " << objects.size() << "\n";
+	std::cout << "Null: " << nulled << "\n";
 
 	while (true)
 	{
